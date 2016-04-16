8fa2f81bcee3679d5d6ef028b04a1469160ecb7d
lol50002 <artem130500@mail.ru>
2016-04-16 22:31:01 +0300

    LOL
    LOL

---
+++ Fibbonachy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(10));
+            Console.ReadKey();
+        }
+        
+
+        public static int Fib(int n)
+        {
+            int[] chisla = new int[100];
+
+            if (n <= 1) return n;
+
+            if (chisla[n] != 0) return chisla[n];
+
+            chisla[n] = Fib(n - 2) + Fib(n - 1);
+            return chisla[n];
+        }
+    }
+}
